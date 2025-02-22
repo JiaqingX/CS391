3822fb3083aa1927666f61b319dbda6d9599663a
Jiaqing Xu <jiaqing@bu.edu>
2025-02-21 23:07:03 -0500

    debug package.json and test

--- MP2/mp-2/src/components/DataFetcher.tsx
+++ MP2/mp-2/src/components/DataFetcher.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import DisplayData from '../components/DisplayData';
+import DisplayData from './DisplayData';
 
 interface Character {
   id: number;
